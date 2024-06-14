e1d86fa73b0a547a3ea9f504e5b37558820e43a8
Jacob O'Neill <jacoboneill2000@outlook.com>
2024-06-14 17:25:03 +0100

    Add command line args

---
+++ 02/notes/command_line_arguments/assets/main.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+string get_name();
+
+int main(int argc, string argv[])
+{
+  string name;
+  if(argc > 1){
+    name = argv[1];
+  } else {
+    name = get_name();
+  }
+
+  printf("Welcome, %s!\n", name);
+}
+
+string get_name(){
+  return get_string("Hello. What is your name?\n");
+}
