ee4336b1e60f091ddf4bfe80f86a56d7db8c4c43
Jacob O'Neill <jacoboneill2000@outlook.com>
2024-08-30 15:06:00 +0100

    Add dynamic sports field and validation of sports

--- 09/notes/assets/froshims/app.py
+++ 09/notes/assets/froshims/app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+SPORTS = ["Basketball", "Football", "Ultimate Frisbee"]
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
 
 @app.route("/register", methods=["POST"])
 def register():
-    if not request.form.get("name") or not request.form.get("sport"):
+    if not request.form.get("name") or not request.form.get("sport") or request.form.get("sport") not in SPORTS:
         return render_template("failure.html")
     return render_template("success.html")
