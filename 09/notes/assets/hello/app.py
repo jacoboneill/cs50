b81aaf874bca963d2a7fd806928d29d90c32ecc4
Jacob O'Neill <jacoboneill2000@outlook.com>
2024-08-30 14:28:05 +0100

    Add form and greet on same / path and fix blank form submission with Jinja conditional

--- 09/notes/assets/hello/app.py
+++ 09/notes/assets/hello/app.py
@@ -2,12 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    name = request.args.get("name", "world")
-    return render_template("index.html", name=name)
+    if request.method == "POST":
+        name = request.form.get("name")
+        return render_template("greet.html", name=name)
 
-@app.route("/greet", methods=["POST"])
-def greet():
-    name = request.args.get("name", "world")
-    return render_template("greet.html", name=name)
+    return render_template("index.html")
+
